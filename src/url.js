676dca1d5f8401862e4f24f45a2069c830d7444b
yago <yagosonicmassaroni@gmail.com>
2024-11-07 16:05:04 -0300

    trocando forma de export

--- src/url.js
+++ src/url.js
@@ -2,14 +2,13 @@ import { nanoid } from "nanoid"
 
 let urlDatabase = {}
 
-const createShortUrl = (originalUrl) => {
+export const createShortUrl = (originalUrl) => {
   const shortUrl = nanoid(8) // Gera um short URL com 8 caracteres
   urlDatabase[shortUrl] = originalUrl
   return shortUrl
 }
 
-const getOriginalUrl = (shortUrl) => {
+export const getOriginalUrl = (shortUrl) => {
   return urlDatabase[shortUrl]
 }
 
-export { createShortUrl, getOriginalUrl }
